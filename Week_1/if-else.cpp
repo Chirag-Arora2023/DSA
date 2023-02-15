6a595e4c447fb29e185f1d0b02623fc3297b01c2
Chirag-Arora2023 <cse.chirag.arora@gmail.com>
2023-02-15 14:30:02 +0530

    28 Jan to 31 Jan

--- Week_1/if-else.cpp
+++ Week_1/if-else.cpp
@@ -1,16 +1,73 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    int age;
-    cout<<"Enter your age : ";  cin>>age;
-    if(age<18)
-    cout<<"You are a student, you shuld focus on studies ";
-
-    else if(age>=18  &&  age<30)
-    cout<<"You are young and at a very critical age, your actions are a double edged sword , so learn to wield it.";
+    // int a=9;
+    // if (a==9)
+    // {
+    //     cout<<"naaney"<<endl;
+    // }
+    // else if (a>0)                                             //OUTPUT IS NAANEY
+    // {
+    //     cout<<"a is positive"<<endl;
+    // }
+    // else{cout<<"a is negative";}
     
-    else cout<<"You are a mature and experienced person who has seen the world";
+    // int a=5, b=a+4;
 
-return 0;
+    // if ((a=9)==b)                                              OUTPUT IS  a
+    // {
+    //     cout<<"a"<<endl;
+    // }else{cout<<"a+4"<<endl;}
+    
+    // int a=7;
+    // if (a>0)
+    // {
+    //     cout<<"greater than 0"<<endl;
+    // }
+    // else if (a==7)
+    // {
+    //     cout<<"a is equal to 7"<<endl;
+    // }
+    
+        // int a,b,c;
+        // cout<<"Enter the value of a "<<endl;                    // WHICH NUMBER IS GREATER
+        // cin>>a;
+        // cout<<"Enter the value of b "<<endl;
+        // cin>>b;
+        // cout<<"Enter the value of c "<<endl;
+        // cin>>c;
+        // if      (a>b&&a>c)
+        //         {cout<<"a is greatest"<<endl;}
+        // else if (b>a&&b>c)
+        //         {cout<<"b is greatest"<<endl;}
+        // else    {cout<<"c is greatest"<<endl;}
+        
+        // bool CPP,workout,read;
+        // cout<<"Did you do programming today"<<endl;              TASK MANAGER
+        // cin>>CPP;
+        // cout<<"Did you wokout today"<<endl;
+        // cin>>workout;
+        // cout<<"Did you read a book today"<<endl;
+        // cin>>read;
+
+        // if (CPP=1 && workout=1 &&read=1)
+        // {
+            
+        // }
+        
+        
+        //    char ch;                                                         //INPUT NUMERIC OR ALPHABET
+        //    cout<<"Enter th echaracter"<<endl;
+        //    cin>>ch;
+
+        //     if       (ch>=48 && ch<58)
+        //             {cout<<"The input character "<<ch<<" is NUMERIC"<<endl;}
+        //     else if(ch>=65 && ch<90)
+        //             {cout<<"The input character "<<ch<<" is UPPERCASE"<<endl;}
+        //     else if(ch>=97 && ch<122)
+        //             {cout<<"The input character "<<ch<<" is LOWERCASE"<<endl;}
+        //     else{cout<<"invalid input"<<endl;}
+
+        
+
 }

0186e2f5aa02e788394d19519a1c9f162280d5de
Chirag Arora <cse.chirag.arora@gmail.com>
2023-02-25 17:44:45 +0530

    sort & search approach, -ve marking approach

---
+++ Week_5/25_FEB/Missing_Element.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr{1,3,5,3,4,7,3,4,9};
+
+//     //APPROACH 1  SORT AND SEARCH
+//     for(int i=0;i<arr.size();i++){
+//         int mini = i;
+//         for(int j=i+1 ; j<arr.size(); j++){
+//             if(arr[j] < arr[mini])
+//                 mini = j;
+//         }
+//         swap(arr[i] , arr[mini]);
+//     }
+//    // for(int i=0;i<arr.size();i++) cout<<arr[i]<<" ";
+
+//     for(int i=0;i<arr.size();i++){
+//         if(arr[i] != i+1)
+//         cout<<"The missing element is " <<i+1<<" "<<endl;
+//     }
+
+    //NEGATIVE MARKING APPROACH
+    vector<int> brr{1,3,5,3,4,3,7,7,7};
+    int n = brr.size();
+    for(int i=0;i<n;i++){
+        int index = abs(brr[i]);
+
+        if(brr[index - 1]>0) 
+        brr[index - 1] *= -1;
+        
+    }
+    for(int i=0;i<n;i++){
+        if(brr[i] > 0)
+        cout<<i+1<<" ";
+    }
+
+
+    return 0;
+}

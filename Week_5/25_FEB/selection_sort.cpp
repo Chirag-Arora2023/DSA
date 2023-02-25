d7989d7b431ec9b4fdc695ed12dd44a2855a87af
Chirag Arora <cse.chirag.arora@gmail.com>
2023-02-25 17:44:05 +0530

    Selection sort basic code

---
+++ Week_5/25_FEB/selection_sort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{1,3,5,3,4};
+    //SORT THE ARRAY USING SELECTION SORT
+    int size = arr.size();
+
+    for(int i=0;i<size;i++){
+        int minIndex = i;
+        for(int j=i+1;j<size;j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i] , arr[minIndex]);
+    }
+    for(int i=0;i<size;i++) cout<<arr[i]<<" ";
+    return 0;
+}

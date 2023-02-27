69d430c6beafdd76164daaf1055f061c393e8d6b
Chirag Arora <cse.chirag.arora@gmail.com>
2023-02-27 08:27:35 +0530

    Finding duplicate using XOR

--- Week_5/26_FEB/Find_Duplicates.cpp
+++ Week_5/26_FEB/Find_Duplicates.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 
 int main(){
-    
+    vector<int> arr{1,2,3,4,2};
+    int ans =0;
+    for(int i=0;i<arr.size();i++){
+        ans^=arr[i];
+    }
+    cout<<ans<<endl;
 
+    for(int i=1;i<arr.size();i++)
+        ans^= i;
+    cout<<(1^2)<<" "<<(2^3)<<" "<<(1^1);
+    cout<<endl<<ans;
     return 0;
 }

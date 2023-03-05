de87c28d6aac901333ce3fe5c5abd21c24cacf20
Chirag Arora <cse.chirag.arora@gmail.com>
2023-03-05 09:42:10 +0530

    .

---
+++ 4_MARCH/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char arr[50];
+    //cin>>arr;
+    //getline(cin,arr);  DOSENT WORK
+    cin>>arr[3];
+    //cin.getline(arr,100);
+    cin >> arr[49];
+    cout<<arr[3];
+    cout<<arr[49];
+    return 0;
+}

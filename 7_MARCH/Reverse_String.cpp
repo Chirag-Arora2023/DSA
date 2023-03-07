b99a9ab374e50c24ee575caa89ba505b69042787
Chirag Arora <cse.chirag.arora@gmail.com>
2023-03-07 20:21:23 +0530

    .

---
+++ 7_MARCH/Reverse_String.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    void reverseString(vector<char>& s) {
+        int l=0 , h=s.size() -1;
+        while(l<h){
+            swap(s[l++] , s[h--]);
+        }
+    }
+
+int main(){
+    return 0;
+}
